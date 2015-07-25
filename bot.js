a89886335bf19e2bab80e7aa9b067ac91b5fc00d
Ryan <xenoputtss@gmail.com>
2015-07-25 10:35:14 -0400

    Control LED brightness with Potentiometer

--- bot.js
+++ bot.js
@@ -2,11 +2,24 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
-  (new five.Led(11)).strobe();
 
   var input = new five.Sensor("A0");
+  var led = new five.Led(11);
+  var initialValue;
 
   input.on("data", function() {
-    console.log(this.value);
+    if(initialValue === undefined) {
+      initialValue = this.value;
+    }
+    setLed(this.value);
   });
+
+  function setLed(value) {
+    // sway +/- points to control the LED
+    var sway = 50;
+    var brightness = five.Fn.map(value, initialValue - sway, initialValue + sway, 0, 255);
+
+    console.log("setting LED to " + brightness);
+    led.brightness(brightness);
+  }
 });
