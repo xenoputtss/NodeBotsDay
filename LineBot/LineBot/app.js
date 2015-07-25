cdb6318561a44bc9c570695442582868f3d7e275
Ryan <xenoputtss@gmail.com>
2015-07-25 13:20:58 -0400

    Create VS project

--- LineBot/LineBot/app.js
+++ LineBot/LineBot/app.js
@@ -1,4 +1,4 @@
-var five = require("johnny-five");
+﻿var five = require("johnny-five");
 var board = new five.Board();
 
 var stdin = process.stdin;
@@ -37,34 +37,34 @@ board.on("ready", function () {
     wheels.stop();
     console.log("Use the cursor keys or ASWD to move your bot. Hit escape or the spacebar to stop.");
     
-    stdin.on("keypress", function(chunk, key) {
+    stdin.on("keypress", function (chunk, key) {
         if (!key) return;
         
         switch (key.name) {
-        case 'up':
-        case 'w':
-            wheels.forward();
-            break;
+            case 'up':
+            case 'w':
+                wheels.forward();
+                break;
             
-        case 'down':
-        case 's':
-            wheels.back();
-            break;
+            case 'down':
+            case 's':
+                wheels.back();
+                break;
             
-        case 'left':
-        case 'a':
-            wheels.pivotLeft();
-            break;
+            case 'left':
+            case 'a':
+                wheels.pivotLeft();
+                break;
             
-        case 'right':
-        case 'd':
-            wheels.pivotRight();
-            break;
+            case 'right':
+            case 'd':
+                wheels.pivotRight();
+                break;
             
-        case 'space':
-        case 'escape':
-            wheels.stop();
-            break;
+            case 'space':
+            case 'escape':
+                wheels.stop();
+                break;
         }
     });
 });
